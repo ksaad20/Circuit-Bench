273bc3b94055e2992f40a9cd40aa2c61e3b58968
Kazi Saad Asif <62234655+ksaad20@users.noreply.github.com>
2026-07-08 23:10:22 +0600

    Update __init__.py

--- src/metrics/regression/__init__.py
+++ src/metrics/regression/__init__.py
@@ -1,89 +1,36 @@
-from .error_metrics import (
-    mean_absolute_error,
-    mean_squared_error,
-    root_mean_squared_error,
-    mean_absolute_percentage_error,
-    huber_loss,
-    quantile_loss,
-)
+"""
+Regression Metrics
 
-from .goodness_of_fit import (
-    r2_score_metric,
-    adjusted_r2,
-    explained_variance,
-    nash_sutcliffe_efficiency,
-    willmott_index,
-)
+Comprehensive regression evaluation utilities for CircuitBench.
 
-from .residuals import (
-    residuals,
-    residual_summary,
-    standardized_residuals,
-)
+Modules
+-------
+error_metrics
+goodness_of_fit
+residuals
+diagnostics
+influence
+uncertainty
+benchmarking
+reporting
+"""
 
-from .diagnostics import (
-    breusch_pagan_test,
-    white_test,
-    shapiro_residual_test,
-)
+from . import error_metrics
+from . import goodness_of_fit
+from . import residuals
+from . import diagnostics
+from . import influence
+from . import uncertainty
+from . import benchmarking
+from . import reporting
 
-from .influence import (
-    cooks_distance,
-    leverage,
-    variance_inflation,
-)
+from .error_metrics import *
+from .goodness_of_fit import *
+from .residuals import *
+from .diagnostics import *
+from .influence import *
+from .uncertainty import *
+from .benchmarking import *
+from .reporting import *
 
-from .uncertainty import (
-    prediction_interval,
-    coverage_probability,
-    uncertainty_report,
-)
-
-from .benchmarking import (
-    rank_models,
-    leaderboard,
-    benchmark_summary,
-)
-
-from .reporting import (
-    regression_report,
-    regression_pipeline,
-)
-
-__all__ = [
-    "mean_absolute_error",
-    "mean_squared_error",
-    "root_mean_squared_error",
-    "mean_absolute_percentage_error",
-    "huber_loss",
-    "quantile_loss",
-
-    "r2_score_metric",
-    "adjusted_r2",
-    "explained_variance",
-    "nash_sutcliffe_efficiency",
-    "willmott_index",
-
-    "residuals",
-    "residual_summary",
-    "standardized_residuals",
-
-    "breusch_pagan_test",
-    "white_test",
-    "shapiro_residual_test",
-
-    "cooks_distance",
-    "leverage",
-    "variance_inflation",
-
-    "prediction_interval",
-    "coverage_probability",
-    "uncertainty_report",
-
-    "rank_models",
-    "leaderboard",
-    "benchmark_summary",
-
-    "regression_report",
-    "regression_pipeline",
-]
+__version__ = "0.1.0"
