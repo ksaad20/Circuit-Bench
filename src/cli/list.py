3f8024e4fe7fbd0045b98f616dfacaef1dcca274
Kazi Saad Asif <62234655+ksaad20@users.noreply.github.com>
2026-07-13 17:03:24 +0600

    Add list.py to display available benchmarks

---
+++ src/cli/list.py
@@ -0,0 +1,10 @@
+"""List available benchmarks."""
+
+
+def execute(args):
+    print("Available Benchmarks")
+    print("--------------------")
+    print("converter_efficiency")
+    print("thermal_prediction")
+    print("analog_filter")
+    print("power_loss")
